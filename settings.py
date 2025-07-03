43d98e49933408cbaf28bd4cc14f1b00a1e361eb
PrinceOdein <odeinanyanwu@gmail.com>
2025-07-03 23:21:19 +0100

    Re-added cleaned settings.py using env vars

---
+++ settings.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+SEARCH_ENGINE_ID = os.getenv("SEARCH_ENGINE_ID")
+
+
+COUNTRY = 'us'
+MAX_RESULTS = 100
